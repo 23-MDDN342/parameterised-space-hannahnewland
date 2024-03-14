dcb9ee05a9b2a69e799efa4af1fe6dc71c2a6e3e
hannahnewland <139179132+hannahnewland@users.noreply.github.com>
2024-03-14 14:18:18 +1300

    Got Ellipses repeating, travelling

--- draw_one_frame.js
+++ draw_one_frame.js
@@ -2,144 +2,44 @@
 let firstRun = true
 let objs = [];
 let colors = ['#DFDFDF', '#E46424', '#E50009', '#14B7DA', '#0881E6', '#FF75A5', '#FCD232', '#800204', '#04E29A', '#333333'];
-
-// function draw_one_frame(cur_frac) {
-// 	if(firstRun){
-// 		rectMode(CENTER);
-// 		strokeWeight(2);
-// 		let c = 8;
-// 		let w = width / c;
-// 		for (let i = 0; i < c; i++) {
-// 			for (let j = 0; j < c; j++) {
-// 				let x = i * w + w / 2;
-// 				let y = j * w + w / 2;
-// 				objs.push(new OBJ(x, y, w));
-// 			}
-// 		}
-
-
-// 		firstRun = false
-// 	}
-
-// 	for (let i of objs) {
-// 		i.show();
-// 		i.move();
-// 	}
-
-
-//  	}
-
-
-
-// function easeInOutQuad(x) {
-// 	return x < 0.5 ? 2 * x * x : 1 - Math.pow(-2 * x + 2, 2) / 2; // this creates the easing effect of the movements, without it they just jump around
-// }
-
-// class OBJ {
-// 	constructor(x, y, w) {
-// 		this.x = x;
-// 		this.y = y;
-// 		this.w = w;
-// 		this.col1 = this.col2 = this.col3;
-// 		while (this.col1 == this.col2 || this.col2 == this.col3 || this.col1 == this.col3) {
-// 			this.col1 = random(colors);
-// 			this.col2 = random(colors);
-// 			this.col3 = random(colors);
-// 		}
-// 		this.ang = int(random(4)) * (TAU / 4); // I think this puts the circles at different positions within the squares.
-
-// 		this.t = -int(random(7)) * 100;
-// 		this.t1 = 50;
-// 		this.t2 = this.t1 + 50;
-// 		this.ss1 = ((this.w * 2) / 3) * 2;
-// 		this.ss2 = ((this.w * 2) / 3);
-// 		this.d1 = this.ss1; // this makes the ss1 shapes actually move
-// 		this.d2 = this.ss2; // this makes the ss2 shapes actually move
-// 	}
-
-// 	show() {
-// 		push();
-// 		translate(this.x, this.y);
-// 		noStroke();
-// 		fill(this.col1);
-// 		square(0, 0, this.w);
-// 		drawingContext.clip(); // this creates the border of the squares; like a clipping mask on Adobe Illustrator
-// 		rotate(this.ang);
-// 		stroke(0);
-// 		fill(this.col2);
-// 		// circle(this.w / 2, this.w / 2, this.d1);
-// 		ellipse(this.w/2,this.w/2, this.d1);
-// 		fill(this.col3);
-// 		//circle(this.w / 2, this.w / 2, this.d2);
-// 		noFill();
-// 		square(0, 0, this.w);
-// 		pop();
-// 	}
-
-// 	move() {
-
-// 		this.t++;
-// 		if (0 < this.t && this.t < this.t1) {
-// 			let n = norm(this.t, 0, this.t1 - 1); // norm maps a number from one range to a value between 0 and 1, i.e. norm(2, 0, 10) returns 0.2 because 2's position [0,10] is proportional to 0.2's position in [0,1].
-// 			this.d1 = lerp(this.ss1, this.ss1 + this.w * 2.5, easeInOutQuad(n));
-// 			this.d2 = lerp(this.ss2, this.ss2 + this.w * 2.5, easeInOutQuad(n));
-// 		}
-// 		if (this.t == this.t1 + 1) {
-// 			this.col1 = this.col3;
-// 			while (this.col1 == this.col2 || this.col2 == this.col3 || this.col1 == this.col3) {
-// 				this.col2 = random(colors);
-// 				this.col3 = random(colors);
-// 			}
-// 			this.ang = int(random(4)) * (TAU / 4);
-// 		}
-// 		if (this.t1 < this.t && this.t < this.t2) {
-// 			let n = norm(this.t, this.t1, this.t2 - 1);
-// 			this.d1 = constrain(lerp(this.ss1 - this.w * 2, this.ss1, easeInOutQuad(n)), 0, this.ss1);
-// 			this.d2 = constrain(lerp(this.ss2 - this.w * 2, this.ss2, easeInOutQuad(n)), 0, this.ss2);
-// 		}
-// 		if (this.t2 <= this.t) {
-// 			this.t = -int(random(7) + 1) * 100;
-// 		}
-// 	}
-// }
+let c = 0;
 
 function draw_one_frame(cur_frac){
 
-	// if(firstRun){
-	// 	rectMode(CENTER);
-	// 	strokeWeight(2);
-	// 	let c = 8;
-	// 	let w = width / c;
-	// 	for (let i = 0; i < c; i++) {
-	// 		for (let j = 0; j < c; j++) {
-	// 			let x = i * w + w / 2;
-	// 			let y = j * w + w / 2;
-	// 			objs.push(new OBJ(x, y, w));
-	// 		}
-	// 	}
+	width = 960;
+	height = 540;
 
+	let w = width;
+	let h = height;
 
-	// 	firstRun = false
-	// }
-	// for (let i of objs) {
-	// 			i.show();
-	// 			i.move();
-	// 		}
-		
+noStroke();
+fill("#000000");
+rect(0,0,w,h);
 
-fill("#E50009");
-rect(0,0,width,height);
+let HowManySquares = 6;
 
-let HowManySquares = 10;
+let SquareSize = w/HowManySquares;
+let SpacingAmount = w/HowManySquares -1 ;
 
-let SquareSize = width/HowManySquares;
-let SpacingAmount = width/HowManySquares +10;
-
-for(let across = 0; across < width / SpacingAmount -1; across++){
-	for(let down = 0; down +1 < height / SpacingAmount; down++ ){
-		fill( "#E46424");
+for(let across =0; across < w / SpacingAmount-1; across++){
+	for(let down = 0; down +1 < h / SpacingAmount; down++ ){
+		//fill(random(colors));
+		stroke("#FFFFFF");
 		rect(SpacingAmount*across, SpacingAmount*down, SquareSize);
+		let animateCircle = map(cur_frac, 0, 1, 0, SpacingAmount);
+		push();
+		for(let ii = 0; ii < SpacingAmount; ii++){
+		fill(random(colors));
+		stroke(random(colors));
+		let TravelEllipse = animateCircle - (ii*1);
+        ellipse(TravelEllipse, 10, HowManySquares*5);
+		ellipse(SpacingAmount+40, TravelEllipse, HowManySquares*5);
+		// ellipse(startX- (ii*1), 50, HowManySquares*5);
+		}
+		pop();
+
 	}
 }
-
 }
+
+
